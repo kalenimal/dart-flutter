43df6d21a9dfc2e7f845c567190e517cf645bd62
Kirill <kirillkalinckin@yandex.ru>
2022-09-21 00:26:47 +0300

    serachScreen bottom section

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -12,7 +12,7 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 40),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         children: [
           const Gap(40),
           Text('What are \n you looking for?',
@@ -95,15 +95,99 @@ class SearchScreen extends StatelessWidget {
           const Gap(20),
           ElevatedButton(
               onPressed: () => print('pressed'),
-              style: ElevatedButton.styleFrom(primary: Colors.blue.shade700,
-              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-              padding: const EdgeInsets.all(10)
-              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.blue.shade700,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: const EdgeInsets.all(10)),
               /* shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)), */
               child: Text(
                 'Find tickets',
                 style: Styles.headlineStyle2.copyWith(color: Colors.white),
+              )),
+          const Gap(40),
+          Container(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Upcoming flights',
+                    style: Styles.headlineStyle2.copyWith(fontSize: 25),
+                  ),
+                  InkWell(
+                    child: Text('View all', style: Styles.headlineStyle3),
+                  )
+                ]),
+          ),
+          const Gap(20),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 350,
+                  clipBehavior: Clip.hardEdge,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Wrap(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.antiAlias,
+                        width: double.infinity,
+                        height: 150,
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images/lake.jpg'))),
+                      ),
+                      const Gap(15),
+                      Text(
+                          'It is a long established fact that a reader will be distracted by the readable content of a page when',
+                          style: Styles.headlineStyle2),
+                    ],
+                  ),
+                ),
+              ),
+              const Gap(10),
+              Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                     padding: const EdgeInsets.all(10), 
+                      decoration: BoxDecoration(
+                          color: Colors.lightGreen.shade200,
+                          borderRadius: BorderRadius.circular(15)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                        Text('Discount\n for survey', style: Styles.headlineStyle2.copyWith(color: Colors.white)),
+                        const Gap(5),
+                        Text('Many desktop publishing packages and web page editors now use Lorem Ipsum a', style: Styles.headlineStyle3.copyWith(color: Colors.white)),
+                      ], 
+                    ),
+                    ),
+                      Positioned(
+                        right: -40,
+                        top: -40,
+                        child: 
+                      Container(
+                          padding: const EdgeInsets.all(30),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(width: 18, color: Colors.lightGreen.shade500),
+                            color: Colors.transparent
+                          ),
+                        ))
+                    ],
+                  )
+                ],
               ))
+            ],
+          )
         ],
       ),
     );
